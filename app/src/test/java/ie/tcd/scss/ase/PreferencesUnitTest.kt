45b718d12b840e10bbf5d1b0d7aacf285b66fb30
Tomin B Azhakathu <tominbazhakathu@gmail.com>
2019-02-13 16:01:14 +0000

    Preference Save Test And Code

--- app/src/test/java/ie/tcd/scss/ase/PreferencesUnitTest.kt
+++ app/src/test/java/ie/tcd/scss/ase/PreferencesUnitTest.kt
@@ -13,7 +13,7 @@ import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
-class PreferencesActivityUnitTest {
+class PreferencesUnitTest {
 
 //    private val context: Context = RuntimeEnvironment.application.getApplicationContext()
 
