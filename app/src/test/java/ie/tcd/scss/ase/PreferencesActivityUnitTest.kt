ef797d6af28a22fcb82941821b09270d8b26b939
Tomin B Azhakathu <tominbazhakathu@gmail.com>
2019-02-13 15:59:34 +0000

    Preference Save test Test

--- app/src/test/java/ie/tcd/scss/ase/PreferencesActivityUnitTest.kt
+++ app/src/test/java/ie/tcd/scss/ase/PreferencesActivityUnitTest.kt
@@ -1,46 +1,104 @@
 package ie.tcd.scss.ase
 
-import android.location.Geocoder
-import ie.tcd.scss.ase.activites.PreferencesActivity
+import android.content.SharedPreferences
 import org.junit.Test
-import android.view.View
-import android.widget.EditText
+import ie.tcd.scss.ase.poko.SharedPreferenceDataClass
+import ie.tcd.scss.ase.utilities.SharedPreferenceHelper
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.Robolectric
 import org.junit.Assert.*
+import org.junit.Before
 import org.mockito.Mockito.*
-import org.mockito.Answers.*
+import org.mockito.BDDMockito.given
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(RobolectricTestRunner::class)
+@RunWith(MockitoJUnitRunner::class)
 class PreferencesActivityUnitTest {
 
-    @Test
-    fun `test setPreferences`() {
-        val activity = Robolectric.setupActivity(PreferencesActivity::class.java)
+//    private val context: Context = RuntimeEnvironment.application.getApplicationContext()
 
-        activity.setPreferences("abcd", "efgh")
-        val pref = activity.applicationContext.getSharedPreferences("myPref", 0)
+    var mockPref1 = SharedPreferenceDataClass("worklocation","work")
+    var mockPref2 = SharedPreferenceDataClass("homelocation","home")
 
-        val mockedPreferencesActivity = mock(PreferencesActivity::class.java)
-        val work = PreferencesActivity.Lat_Lng(23.01, 23.01)
-        val home = PreferencesActivity.Lat_Lng(24.01, 24.01)
-        `when`(mockedPreferencesActivity.getLocFromAddress("abcd")).thenReturn(work)
-        `when`(mockedPreferencesActivity.getLocFromAddress("efgh")).thenReturn(home)
-        `when`(mockedPreferencesActivity.showNotification())
+    lateinit var prefernceData:ArrayList<SharedPreferenceDataClass>
+    lateinit var sharedPreferenceHelper: SharedPreferenceHelper
 
-        val worLoc = pref.getString("work_location", "")
-        val homeLoc = pref.getString("home_location", "")
-        val worLocLong = pref.getString("work_location_long", "")
-        val worLocLat = pref.getString("work_location_lat", "")
-        val homeLocLong = pref.getString("home_location_long", "")
-        val homeLocLat = pref.getString("home_location_lat", "")
+    @Mock lateinit var sharedPreference : SharedPreferences
+    @Mock lateinit var mockEditor: SharedPreferences.Editor
 
-        assertEquals(worLoc, "abcd")
-        assertEquals(homeLoc, "efgh")
-        assertEquals(worLocLong, 23.01)
-        assertEquals(worLocLat, 23.01)
-        assertEquals(homeLocLong, 24.01)
-        assertEquals(homeLocLat, 24.01)
+
+    @Before
+    fun initMock(){
+        prefernceData = ArrayList()
+        prefernceData.add(mockPref1)
+        prefernceData.add(mockPref2)
+
+//        sharedPreference =  PreferenceManager.getDefaultSharedPreferences(context)
+        sharedPreferenceHelper = createSharedPreferenceHelper()
+
     }
+
+    @Test
+    fun testSavePreferenceHelper(){
+        assertTrue(sharedPreferenceHelper.savePreference(prefernceData))
+
+        val workData = sharedPreferenceHelper.getPreference(mockPref1.key)
+        val homeData = sharedPreferenceHelper.getPreference(mockPref2.key)
+
+        assertEquals(mockPref1.value,workData)
+        assertEquals(mockPref2.value,homeData)
+
+    }
+
+    private fun createSharedPreferenceHelper(): SharedPreferenceHelper {
+
+        given(sharedPreference.getString(eq(mockPref1.key), anyString())).willReturn(mockPref1.value)
+        given(sharedPreference.getString(eq(mockPref2.key), anyString())).willReturn(mockPref2.value)
+
+        given(mockEditor.commit()).willReturn(true)
+
+        // Return the MockEditor when requesting it.
+        given(sharedPreference.edit()).willReturn(mockEditor)
+        return SharedPreferenceHelper(sharedPreference)
+
+    }
+
+
+
+//
+//    @Test
+//    fun testSetPreferences() {
+//
+//
+//
+//
+//
+//        val activity = Robolectric.setupActivity(PreferencesActivity::class.java)
+//
+//        activity.setPreferences("abcd", "efgh")
+//        val pref = activity.applicationContext.getSharedPreferences("myPref", 0)
+//
+//        val mockedPreferencesActivity = mock(PreferencesActivity::class.java)
+//        val work = PreferencesActivity.Lat_Lng(23.01, 23.01)
+//        val home = PreferencesActivity.Lat_Lng(24.01, 24.01)
+//        `when`(mockedPreferencesActivity.getLocFromAddress("abcd")).thenReturn(work)
+//        `when`(mockedPreferencesActivity.getLocFromAddress("efgh")).thenReturn(home)
+//        `when`(mockedPreferencesActivity.showNotification())
+//
+//        val worLoc = pref.getString("work_location", "")
+//        val homeLoc = pref.getString("home_location", "")
+//        val worLocLong = pref.getString("work_location_long", "")
+//        val worLocLat = pref.getString("work_location_lat", "")
+//        val homeLocLong = pref.getString("home_location_long", "")
+//        val homeLocLat = pref.getString("home_location_lat", "")
+//
+//        assertEquals(worLoc, "abcd")
+//        assertEquals(homeLoc, "efgh")
+//        assertEquals(worLocLong, 23.01)
+//        assertEquals(worLocLat, 23.01)
+//        assertEquals(homeLocLong, 24.01)
+//        assertEquals(homeLocLat, 24.01)
+//    }
+
+
 }
