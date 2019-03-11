89d7c856b6e4cb0f8a6b351762918a0c51341be8
akashdeep-singh <dj.akashds@gmail.com>
2019-03-11 14:39:19 +0000

    Cleanup on Main.kt

--- falconserver/src/main/kotlin/Main.kt
+++ falconserver/src/main/kotlin/Main.kt
@@ -1,24 +1,14 @@
 import handlers.ApiRole
 import io.javalin.Javalin
-import services.Weather
-import models.weather.WeatherModel
-import services.BikeStand
-import handlers.Authentication
-import handlers.BikeStandController
-import handlers.WeatherController
+import handlers.*
 import io.javalin.security.SecurityUtil.roles
 
-//data class FalconResponse(val status: Int, val message: WeatherModel)
 
 fun main(args: Array<String>) {
-//    val app = Javalin.create().start(7000)
-
-
     val app = Javalin.create().apply {
         accessManager(Authentication::accessManager)
     }.start(7000)
 
     app.get("/weather", WeatherController::getWeatherByCoordinates, roles(ApiRole.USER_READ))
     app.get("/bikestand", BikeStandController::getRealTimeStandsInfoByCity, roles(ApiRole.USER_READ))
-
 }
