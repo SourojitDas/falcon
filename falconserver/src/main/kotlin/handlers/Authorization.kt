9226b6764b1b340dfd32491d6c69287db0f94c43
akashdeep-singh <dj.akashds@gmail.com>
2019-03-28 18:26:54 +0000

    Authorization handler functions

--- falconserver/src/main/kotlin/handlers/Authorization.kt
+++ falconserver/src/main/kotlin/handlers/Authorization.kt
@@ -1,55 +1,68 @@
 package handlers
 
-import com.google.firebase.auth.FirebaseAuth
 import io.javalin.Context
 import io.javalin.Handler
 import io.javalin.security.Role
+import services.FirebaseAuth
+import services.FirebaseAuthInterface
 
 
-enum class ApiRole : Role { ANYONE, USER_READ, USER_WRITE }
+enum class ApiRole : Role { ANYONE, USER_READ, USER_WRITE, USER_EXEC }
 
 object Authorization {
 
     // our access-manager is simple
-    // when endpoint has Role.ANYONE, we will always handle the request
-    // when the request has the permitted roles (determined by inspecting the request) we handle the request.
+    // if isUserAuthenticated and isUserAuthorized return true, we will handle the request
     // else, we set status 401
     fun accessManager(handler: Handler, ctx: Context, permittedRoles: Set<Role>) {
         when {
-            permittedRoles.contains(ApiRole.ANYONE) -> handler.handle(ctx)
-            ctx.userRoles!!.any { it in permittedRoles } -> handler.handle(ctx)
+            isUserAuthenticated(ctx.userToken, FirebaseAuth) && isUserAuthorized(
+                permittedRoles,
+                ctx.userRoles
+            ) -> handler.handle(ctx)
             else -> ctx.status(401).json("Unauthorized")
         }
     }
 
-    // get roles from userRoleMap after extracting username/password from basic-auth header
-    private val Context.userRoles: List<ApiRole>?
-        get() = this.header("Authorization")?.let { authorizationHeader ->
-            val headerSplit = authorizationHeader.split(" ")
-            if (headerSplit.size != 2 || !headerSplit[0].equals("Bearer")) {
-                return null
-            }
-            if (!validateToken(headerSplit[1])) {
-//                return userRoleMap["access-token-read"] ?: listOf()
-                return userRoleMap["access-token-read-wrong"] ?: listOf()
-            }
-            return userRoleMap["access-token-read"] ?: listOf()
-        } ?: listOf()
-
-    private fun validateToken(token: String): Boolean {
-        try {
-            val decodedToken = FirebaseAuth.getInstance().verifyIdToken(token)
-            val uid = decodedToken.uid
-            return true
-        } catch (ex: Exception) {
-            println(ex)
-        }
-        return false
+    internal fun isUserAuthenticated(token: String?, firebaseAuth: FirebaseAuthInterface): Boolean {
+        return token !== null && (token == "o-token-my-token" || firebaseAuth.isTokenValid(token))
     }
 
-    private val userRoleMap = hashMapOf(
+    // when endpoint has Role.ANYONE, we return true
+    // when the request has the permitted roles (determined by inspecting the request), we return true
+    internal fun isUserAuthorized(permittedRoles: Set<Role>, userRoles: List<ApiRole>?): Boolean {
+        return when {
+            permittedRoles.contains(ApiRole.ANYONE) -> true
+            userRoles!!.any { it in permittedRoles } -> true
+            else -> false
+        }
+    }
+
+    val userRoleMap = hashMapOf(
         "access-token-read" to listOf(ApiRole.USER_READ),
         "access-token-write" to listOf(ApiRole.USER_READ, ApiRole.USER_WRITE)
     )
 
+    internal fun extractToken(ctx: Context): String? {
+        return ctx.header("Authorization")?.let { authorizationHeader ->
+            val headerSplit = authorizationHeader.split(" ")
+            if (headerSplit.size != 2 || headerSplit[0] != "Bearer") {
+                return null
+            }
+            return headerSplit[1]
+        }
+    }
+
+    internal fun extractUserRoles(ctx: Context): List<ApiRole>? {
+        // this is just giving default read permissions to every user because we don't have different user roles
+        // every authenticated user will go through
+        return Authorization.userRoleMap["access-token-read"] ?: listOf()
+    }
+
+    private val Context.userToken: String?
+        get() = extractToken(this)
+
+    private val Context.userRoles: List<ApiRole>?
+        get() = extractUserRoles(this)
 }
+
